530d679587970c336c4f67bc6ff9d42941c1dc79
prashantpunia0001 <prashantpunia999@gmail.com>
2026-08-07 21:30:17 +0530

    Day 2 - Intro Animation JS

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
-// Project Infinity Ultimate
-console.log("Project Infinity Ultimate Started ❤️");
+document.addEventListener("DOMContentLoaded", () => {
+
+const startBtn = document.getElementById("startBtn");
+const hearts = document.querySelector(".hearts");
+const loading = document.getElementById("loading-screen");
+
+// Floating Hearts
+function createHeart(){
+
+    const heart = document.createElement("div");
+
+    heart.innerHTML = "💖";
+
+    heart.style.position = "absolute";
+    heart.style.left = Math.random()*100 + "vw";
+    heart.style.bottom = "-40px";
+    heart.style.fontSize = (20 + Math.random()*25) + "px";
+    heart.style.opacity = Math.random();
+
+    heart.style.transition = "transform 6s linear, opacity 6s linear";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.style.transform = "translateY(-110vh)";
+        heart.style.opacity = "0";
+    },100);
+
+    setTimeout(()=>{
+        heart.remove();
+    },6500);
+
+}
+
+setInterval(createHeart,700);
+
+// Start Button
+
+startBtn.addEventListener("click",()=>{
+
+    loading.style.transition="1s";
+
+    loading.style.transform="scale(.9)";
+    loading.style.opacity="0";
+
+    setTimeout(()=>{
+        alert("✨ Chapter 2 Coming Soon...");
+    },1000);
+
+});
+
+});
